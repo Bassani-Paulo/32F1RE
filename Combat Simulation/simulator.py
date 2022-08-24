0f14737d95b24190bef7efe135af2227f2c55c0d
Paulo <bassanipaulo@gmail.com>
2022-08-24 17:49:03 -0300

    Update simulator.py

--- Combat Simulation/simulator.py
+++ Combat Simulation/simulator.py
@@ -1,10 +1,18 @@
 import csv
 import json
+from json import JSONEncoder
+from types import SimpleNamespace as Namespace
+
+itemsFile = open('/Users/macloving/Documents/GitHub/32F1RE/Game/Items.json')
+
+data = json.load(itemsFile)
+
+class Player:
+    def
 
 
+dataSetFile = open('/Users/macloving/Desktop', 'w')
 
-f = open('Users/macloving/Desktop', 'w')
-
-writer = csv.writer(f)
+writer = csv.writer(dataSetFile)
 
 writer.writerow([""])
